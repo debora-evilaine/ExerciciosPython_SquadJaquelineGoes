f2deeebbcca56a6859ade56438020fe654381474
debora-evilaine <sousxdebora@gmail.com>
2024-01-18 10:23:47 -0300

    Resolvendo soma

--- Ex - Conceitos basicos/ex1.py
+++ Ex - Conceitos basicos/ex1.py
@@ -1,1 +1,13 @@
-# arquivo de teste para ver se o comit está funcionando
+"""
+Peça dois números e realize as principais operações.
+"""
+#SOMA
+num1, num2 = (input("Entre com dois números separados por espaço: ").split())
+# O split é utilizado para separar os números, que são recebidos como uma única string, 
+# em duas strings.
+
+num1 = int(num1) # Agora, transformamos essas duas strings em ints e associamos às 
+                 # variáveis corretas.
+num2 = int(num2)
+
+print(f"A soma de {num1} com {num2} é: ", num1 + num2)
