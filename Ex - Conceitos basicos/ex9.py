45df1f590483dc4509458aafebfb80dbc41d6dc8
debora-evilaine <sousxdebora@gmail.com>
2024-01-18 13:43:07 -0300

    Resolvendo exercicio 9

---
+++ Ex - Conceitos basicos/ex9.py
@@ -0,0 +1,12 @@
+"""
+n de horas de exercicio fisico por semana. queima-se 5 calorias por minuto.
+
+calcule as calorias queimadas no mês. 
+"""
+
+horas_de_exercicio = int(input("Entre com o total de horas de exercício físico na semana: "))
+
+calorias_queimadas = (horas_de_exercicio * 300) * 4
+
+print("O total de calorias queimadas no mês é: ", calorias_queimadas)
+
