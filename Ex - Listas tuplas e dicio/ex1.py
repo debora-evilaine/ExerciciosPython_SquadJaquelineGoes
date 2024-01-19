7827aaee3a2134d69656ac997fd4294dc1c58f14
debora-evilaine <sousxdebora@gmail.com>
2024-01-19 12:55:02 -0300

    Resolvendo exercicio 1 de tuplas e listas

--- Ex - Listas tuplas e dicio/ex1.py
+++ Ex - Listas tuplas e dicio/ex1.py
@@ -1,1 +1,50 @@
-# arquivo de teste para ver se o comit está funcionando
+"""ExercíciosTuplas,ListaseDicionários1.
+Utilizandolistasfaçaumprogramaquefaça5perguntasparaumapessoasobreumcrime.
+Asperguntassão:Telefonouparaavítima?
+Estevenolocaldocrime?Morapertodavítima?Deviaparaavítima?
+Játrabalhoucomavítima?
+Oprogramadevenofinalemitirumaclassificaçãosobreaparticipaçãodapessoanocrime.
+Seapessoaresponderpositivamentea2questõeseladeveserclassificadacomo""Suspeita"",
+entre3e4como""Cúmplice""e5como""Assassino"".Casocontrário,eleseráclassificadocomo""
+Inocente"""
+
+print("Atenção: Todas as respostas devem ser somente 's' ou 'n'")
+
+respostas = []
+
+pergunta = input("Telefonou para a vítima? ")
+
+respostas.append(pergunta)
+
+pergunta = input("Esteve no local do crime? ")
+
+respostas.append(pergunta)
+
+
+pergunta = input("Mora perto da vítima? ")
+
+respostas.append(pergunta)
+
+
+pergunta = input("Devia para a vítima? ")
+
+respostas.append(pergunta)
+
+
+pergunta = input("Já trabalhou com a vítima? ")
+
+respostas.append(pergunta)
+
+resposta_positiva = respostas.count('s')
+
+if resposta_positiva == 2:
+    print("Você é um suspeito! >:(")
+
+elif resposta_positiva > 2 and resposta_positiva <= 4:
+    print("Você é um cúmplice!")
+
+elif resposta_positiva == 5:
+    print("Você é um assassino, e está preso! >:(")
+
+else:
+    print("Você é inocente! :)")
