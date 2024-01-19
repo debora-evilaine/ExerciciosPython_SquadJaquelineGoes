0ca1badfb87ea0fabd83aca1a1cc59afcda9e35b
debora-evilaine <sousxdebora@gmail.com>
2024-01-19 13:02:17 -0300

    Resolvendo o exercicio 6 de tuplas e listas e dicio

---
+++ Ex - Listas tuplas e dicio/ex6.py
@@ -0,0 +1,7 @@
+nome_do_usuario = input("Entre com o seu nome: ")
+
+nome_em_uppercase = nome_do_usuario.upper()
+
+nome_invertido = nome_em_uppercase[::-1]
+
+print("O seu nome invertido é:", nome_invertido)
