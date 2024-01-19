c7b63b85c9feb7186909f1b02e796acf41849bae
debora-evilaine <sousxdebora@gmail.com>
2024-01-19 13:11:16 -0300

    Exercicio 5 de tuplas nao finalizado

---
+++ Ex - Listas tuplas e dicio/ex5.py
@@ -0,0 +1,13 @@
+valor1 = input("Entre com o valor a ser adicionado à primeira tupla: ")
+
+tupla1 = (valor1)
+
+valor2 = input("Entre com o valor a ser adicionado à segunda tupla: ")
+
+tupla2 = (valor2)
+
+espaco = (' ')
+
+tupla_concatenada = tupla1 + espaco + tupla2
+
+print(tupla_concatenada)
